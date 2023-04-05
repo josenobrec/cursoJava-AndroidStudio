3ecb7ff6fa3456fca28907dfaee30eba84aafc96
José Nobre <jnobrecf@gmail.com>
2023-04-05 13:53:55 -0300

    :construction: Slider Projeto

---
+++ SliderProjeto/app/src/main/java/com/cursoandroid/sliderprojeto/MainActivity.java
@@ -0,0 +1,59 @@
+package com.cursoandroid.sliderprojeto;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.heinrichreimersoftware.materialintro.app.IntroActivity;
+import com.heinrichreimersoftware.materialintro.slide.FragmentSlide;
+import com.heinrichreimersoftware.materialintro.slide.SimpleSlide;
+
+public class MainActivity extends IntroActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        //setContentView(R.layout.activity_main);
+
+        /*
+        setButtonBackVisible(false);
+        setButtonNextVisible(false);
+
+        addSlide(new SimpleSlide.Builder()
+                .title("Titulo")
+                .description("Descrição")
+                .image(R.drawable.um)
+                .background(android.R.color.holo_orange_light)
+                .build()
+        );
+
+        addSlide(new SimpleSlide.Builder()
+                .title("Titulo2")
+                .description("Descrição2")
+                .image(R.drawable.dois)
+                .background(android.R.color.holo_orange_light)
+                .build()
+        );
+
+        addSlide(new SimpleSlide.Builder()
+                .title("Titulo3")
+                .description("Descrição3")
+                .image(R.drawable.tres)
+                .background(android.R.color.holo_orange_light)
+                .build()
+        );*/
+
+        addSlide(new FragmentSlide.Builder()
+                .background(android.R.color.white)
+                .fragment(R.layout.intro_1)
+                .build()
+        );
+
+        addSlide(new FragmentSlide.Builder()
+                .background(android.R.color.white)
+                .fragment(R.layout.intro_2)
+                .build()
+        );
+
+    }
+}
